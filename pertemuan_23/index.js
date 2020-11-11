f9a58a24c834c5158cf4e4629e2a45a5630cdb8f
jovanvenly <s21910065@student.unklab.ac.id>
2020-11-11 20:46:43 +0700

    first java

---
+++ pertemuan_23/index.js
@@ -0,0 +1,60 @@
+
+// console.log("hello javascript");
+
+// let umur = 25;
+// _umur = 26;
+// let nilai =70.5;
+// let sudahMenikah = false;
+// let nama = "jovan venly";
+
+// console.log(umur);
+// console.log(nama);
+
+// let alamat;
+// console.log(alamat);
+
+// const tahunLahir = 2010;
+// console.log(tahunLahir);
+
+//OPERATOR
+//ARTMATIKA TAMABAH KURANG BAGI KALI
+
+// let angka1 =10;
+// let angka2 =20;
+// console.log(angka1 * angka2);
+
+// let umur = 19;
+// let namaDepan ="Jovan";
+// let namaBelakang ="venly";
+// console.log(
+//     "Nama Saya" +
+//     namaDepan +
+//     " " + 
+//     namaBelakang + 
+//     ".Umur saya adalah "+
+//      umur +
+//      "tahun"
+//      );
+
+// OPERATOR PERBANDINGAN
+
+// let angka = 10;
+// let angka2 = "10";
+
+// console.log(angka == angka2);
+
+// //ternary operator
+// let angka = 5;
+// let hasil = angka<0? "Negatif" : "Positif";
+// console.log = (hasil);
+
+//Prompt = pengaksesan data/meginput data dari user seperti scanf
+//  let angka = prompt ("Masukkan angka : ");
+//  console.log ("angka yang dimasukkan adalah" + angka);
+
+ let angka1 = prompt("Masukkan berat badan Anda : ");
+
+ let angka2 = prompt("Masukkan Tinggi badan anda : ");
+
+ let BMI =  (angka2 * angka2)/ angka1;
+ console.log ("BMI adalah "+BMI );
---
+++ pertemuan_24/index.js
@@ -0,0 +1,25 @@
+// console.log("Basic JavaScript");
+
+// //Conditional javaScript
+
+// let firstname = "jovan";
+// let isMarried = false;
+
+// if (isMarried)
+// {
+//     console.log(firstName +"is Married");
+// }else {
+//     console.log(firsName +"is not Married")
+// }
+
+// /*Jika umur dibawah 13 tahun maka kategori anak anak 
+// dan jikan umur 13 sampe 16 thn kategori remaja
+// dan jika umur 17-24 keatas kategori pemuda
+// dan jika umur 25 sampai keatas kategori  orang tua 
+// */
+
+// if(umur < 13){
+//     console.log("Kategori anak-anak");
+// }else if(umur >= 13 && umur< 16){
+//     console.log("kategori reamaja");
+// }else if (umur >= 17)
